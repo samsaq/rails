284e4befd7b78cfec706ffb75bedfcd4d3bbc93e
samsaq <sameereza@gmail.com>
2023-06-28 21:25:27 -0700

    removing example files & adding footer

--- qwik-seasonTracker/src/routes/layout.tsx
+++ qwik-seasonTracker/src/routes/layout.tsx
@@ -1,10 +1,21 @@
 import { component$, Slot, useStyles$ } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
+import type { RequestHandler } from '@builder.io/qwik-city';
 
 import Header from '~/components/starter/header/header';
-import Footer from '~/components/starter/footer/footer';
 
 import styles from './styles.css?inline';
+
+export const onGet: RequestHandler = async ({ cacheControl }) => {
+  // Control caching for this request for best performance and to reduce hosting costs:
+  // https://qwik.builder.io/docs/caching/
+  cacheControl({
+    // Always serve a cached response by default, up to a week stale
+    staleWhileRevalidate: 60 * 60 * 24 * 7,
+    // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
+    maxAge: 5,
+  });
+};
 
 export const useServerTimeLoader = routeLoader$(() => {
   return {
@@ -20,7 +31,11 @@ export default component$(() => {
       <main>
         <Slot />
       </main>
-      <Footer />
+      <footer class="footer footer-center p-4 bg-base-200 text-base-content fixed inset-x-0 bottom-0">
+        <div>
+          <p>Made by samsaq#2860 | <a class = "underline underline-offset-2" href="https://github.com/samsaq">Github</a></p>
+        </div>
+      </footer>
     </>
   );
 });
