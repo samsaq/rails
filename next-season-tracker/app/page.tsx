9a1d6077b8aaad9f493a48c1b0502c2ed7b60414
samsaq <sameereza@gmail.com>
2023-07-14 16:42:17 -0700

    Adding prettier & prettier-plugin-tailwindcss

--- next-season-tracker/app/page.tsx
+++ next-season-tracker/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import ChallengeCard from '@/components/challengeCard/challengeCard'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-base-100">
-      
+      <ChallengeCard name="Into the Depths I" week="Week 1" description="test" icon="Into the Depths I.jpg" rewardItems={[]} objectives={[]} />
     </main>
   )
 }
--- next-season-tracker/components/challengeCard/challengeCard.tsx
+++ next-season-tracker/components/challengeCard/challengeCard.tsx
@@ -1,31 +1,36 @@
 import styles from './challengeCard.module.scss';
 import Image from 'next/image';
 
-interface challengeProps { //component props
+interface challengeProps {
+  //component props
+  name: string;
+  week: string;
+  description: string;
+  icon: string; //the icon's file name
+  rewardItems: {
     name: string;
-    week: string;
     description: string;
-    icon: string; //the icon's file name
-    rewardItems: {
-        name: string;
-        description: string;
-        quantity: string;
-      }[];
-      objectives: {
-        name: string;
-        startValue: string;
-        completionValue: string;
-      }[];
+    quantity: string;
+  }[];
+  objectives: {
+    name: string;
+    startValue: string;
+    completionValue: string;
+  }[];
 }
 
-
 export default function ChallengeCard(props: challengeProps) {
-
-    return (
-        <div className= "min-h-full flex flex-col">
-            <Image src={`/seasonalData/seasonChallengesData/seasonChallengeIcons/${props.icon}`} width={84} height={84} alt="Challenge Icon" />
-            <span className=''>props.name</span>
-        </div>
-    )
-
+  return (
+    <div className='flex aspect-square h-40 flex-col items-center border-2 border-solid border-accent-content'>
+      <Image
+        src={`/seasonalData/seasonChallengesData/seasonChallengeIcons/${props.icon}`}
+        width={84}
+        height={84}
+        alt='Challenge Icon'
+      />
+      <span className='truncate font-neue text-sm'>
+        {props.week} \\ {props.name}
+      </span>
+    </div>
+  );
 }
